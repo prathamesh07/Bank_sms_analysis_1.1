fd69458e1fa0e6d0e5c029531adff2889c14dc7e
iisha <iisha@thinkanalytics.in>
2016-07-01 17:44:39 +0530

    my initial commit

--- utility_sms_analysis_main.py
+++ utility_sms_analysis_main.py
@@ -6,7 +6,7 @@ from function_definitions.sms_level1_classification_func import utility_sms_filt
 ti = time()*1000 
 
 #Filtering utility sms from all sms
-bank_sms_df = bank_sms_filtering_func("user_sms_pipe.csv") # creates a 'bank_sms_raw.csv' file
+bank_sms_df = utility_sms_filtering_func("user_sms_pipe.csv") # creates a 'bank_sms_raw.csv' file
 
 tf = time()*1000
 
